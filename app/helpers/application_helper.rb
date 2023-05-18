64208b888294be00bdc4535a0d356d6574859573
Yetrina Battad <hello@yetti.io>
2023-05-18 11:23:41 +1000

    fix: navigate to catalogue via header logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
+require "uri"
+
 module ApplicationHelper
   include Blacklight::LocalePicker::LocaleHelper
 
   def additional_locale_routing_scopes
     [blacklight, arclight_engine]
+  end
+
+  def nla_catalogue_url
+    "#{request.protocol}#{request.host}"
   end
 end
