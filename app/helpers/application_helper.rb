11735e97046fd96671ae75185eafc3f4f1e672f8
Yetrina Battad <hello@yetti.io>
2023-05-23 11:04:09 +1000

    chore: cleanup code coverage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,11 @@ require "uri"
 module ApplicationHelper
   include Blacklight::LocalePicker::LocaleHelper
 
+  # :nocov:
   def additional_locale_routing_scopes
     [blacklight, arclight_engine]
   end
+  # :nocov:
 
   def nla_catalogue_url
     "#{request.protocol}#{request.host}"
