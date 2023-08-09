21b3f7babd82c2258af3134f1754ce13866f6ea4
Yetrina Battad <hello@yetti.io>
2023-08-09 14:58:37 +1000

    fix: apply the same styles as in blacklight

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,31 @@ import "controllers"
 
 Turbo.session.drive = false
 
+$(function() {
+  let blacklightModal = $('#blacklight-modal');
+  let modalObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      const newNodes = mutation.addedNodes;
+      if (newNodes !== null) {
+        let plotConfig = blacklightModal.find('.blrl-plot-config');
+        if (plotConfig) {
+          plotConfig.data('plot-config', {
+            selection: { color: '#46474A' },
+            colors: ['#ffffff'],
+            series: { lines: { fillColor: 'rgba(112,57,150, 0.8)' }},
+            grid: { color: '#677078', tickColor: '#f4f5f6', borderWidth: 1 }
+          });
+        }
+      }
+    });
+  });
+  let observerConfig = {
+    childList: true,
+    subtree: true
+  }
+  modalObserver.observe(blacklightModal[0], observerConfig);
+});
+
 Blacklight.onLoad(function() {
 
   /* Searching within a collection should not yield results */
