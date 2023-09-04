8357a17c7d2a737495a56b06622f38c0629170d5
Yetrina Battad <hello@yetti.io>
2023-09-05 08:22:02 +1000

    revert: remove @rails/ujs in favour of turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-import Rails from "@rails/ujs"
-
-// rails-ujs backwards compatibility
-Rails.start();
