fd4d8bddc7e8a962da9bdc96faea5e2351826f8a
Yetrina Battad <hello@yetti.io>
2023-09-20 10:14:01 +1000

    feat: modify dev port and logo link

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -22,6 +22,9 @@ class CatalogController < ApplicationController
     ## Model that maps search index responses to the blacklight response model
     # config.response_model = Blacklight::Solr::Response
 
+    ## The destination for the link around the logo in the header
+    config.logo_link = "https://www.nla.gov.au"
+
     ## Default parameters to send to solr for all search-like requests. See also SearchBuilder#processed_parameters
     config.default_solr_params = {
       rows: 10,
