c498d8bb46c75b72ad1d57abb7ad5e280888b81b
Yetrina Battad <hello@yetti.io>
2023-11-17 10:00:17 +1100

    feat: configure root to NLA collection search results

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,13 @@ Rails.application.routes.draw do
     mount Arclight::Engine => "/"
     mount Yabeda::Prometheus::Exporter => "/metrics"
 
-    root to: "arclight/repositories#index" # "catalog#index", defaults: {f: {level: ["Collection"], repository: ["National Library of Australia"]}}
     concern :searchable, Blacklight::Routes::Searchable.new
+    concern :exportable, Blacklight::Routes::Exportable.new
+    concern :hierarchy, Arclight::Routes::Hierarchy.new
 
     resource :catalog, only: [:index], as: "catalog", path: "/catalog", controller: "catalog" do
       concerns :searchable
     end
-
-    concern :exportable, Blacklight::Routes::Exportable.new
-    concern :hierarchy, Arclight::Routes::Hierarchy.new
 
     resources :solr_documents, only: [:show], path: "/catalog", controller: "catalog" do
       concerns :hierarchy
@@ -26,6 +24,15 @@ Rails.application.routes.draw do
         delete "clear"
       end
     end
+
+    # Show repostories with information about each
+    # root to: "arclight/repositories#index"
+
+    # Show first 100 NLA collections
+    # root to: "arclight/repositories#show", id: "nla"
+
+    # Show search for NLA collections
+    root to: "catalog#index", defaults: {f: {level: ["Collection"], repository: ["National Library of Australia"]}}
   end
 
   get "/", to: redirect("/finding-aids")
