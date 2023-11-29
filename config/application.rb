0ad69e5940ca806b5b856a425f774c5e08fcfbfb
Yetrina Battad <12688+yetti@users.noreply.github.com>
2023-11-29 14:42:47 +1100

    chore(main): release 2.7.1

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ if %w[development staging production].include? ENV["RAILS_ENV"]
 end
 
 module NlaArclight
-  VERSION = "2.7.0"
+  VERSION = "2.7.1"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
