7de0c8185216b8bdbbf0d435b65f3f6c1431b7c3
CEdwardsBlasikiewicz <cedwards@nla.gov.au>
2024-04-18 12:28:54 +1000

    Combine UnitId and Level into one field, with placeholder name "UnitLevel" until RS provides desired name.

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -8,6 +8,10 @@ class SolrDocument
   # alias this field for the request link
   attribute :bibid, :string, "bibid_ssi"
 
+  def unit_level
+    "#{self['level_ssm'].join(' ')}, #{self['unitid_ssm'].join(' ')}"
+  end
+
   # self.unique_key = 'id'
 
   # DublinCore uses the semantic field mappings below to assemble an OAI-compliant Dublin Core document
