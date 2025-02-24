3da966bd560d58bbedd1f7b81f81f4e1ea275d27
CEdwardsBlasikiewicz <cedwards@nla.gov.au>
2025-02-25 08:23:52 +1100

    fix deploy issues

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -26,7 +26,7 @@ fi
 gem install bundler
 bundle config --local jobs $(nproc)
 bundle config --local path "vendor/bundle"
-bundle config --local build.nokogiri --use-system-libraries
+bundle config set force_ruby_platform true
 
 if [[ "$RAILS_ENV" == "staging" || "$RAILS_ENV" == "production" ]]; then
  bundle config --local without "development:test"
