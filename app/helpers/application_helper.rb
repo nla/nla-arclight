23c1774e994f06ba93b2a1c6f6d8d80cce5175e8
CEdwardsBlasikiewicz <cedwards@nla.gov.au>
2026-04-14 10:41:33 +1000

    stop eresources guest banner being shown for logged in patrons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,15 @@ module ApplicationHelper
     [blacklight, arclight_engine]
   end
 
+  def eresources_link_url
+    ebsco_eresources_url = ENV["EBSCO_ERESOURCES_URL"]
+    if current_user.present? && ebsco_eresources_url.present?
+      ebsco_eresources_url
+    else
+      ENV.fetch("ERESOURCES_GUEST_URL", "https://www.nla.gov.au/eresources")
+    end
+  end
+
   def show_analytics?
     response.status != 404 || !routing_error?
   end
