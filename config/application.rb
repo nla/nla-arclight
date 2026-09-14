f39062166a0b8fccf75087898bc154b40b12e759
Calum Edwards Blasikiewicz <153031672+CEdwardsBlasikiewicz@users.noreply.github.com>
2026-09-14 10:43:34 +1000

    Post CTPL26-3 release bump (#943)
    * prep release
    
    * post release

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ if %w[development test].include? ENV["RAILS_ENV"]
 end
 
 module NlaArclight
-  VERSION = "4.6.0"
+  VERSION = "4.7.0"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
